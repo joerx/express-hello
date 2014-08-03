742adf46af7ec710e6713992cdf7914d5499ad4c
Joerg Henning <henning.joerg@gmail.com>
2014-08-03 11:53:16 +0700

    host

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
+var format = require('util').format;
 
 var app = express();
 app.get('/', function(req, res) {
-  res.status(200).json({msg: 'Hello World!'});
+  res.status(200).json({msg: format('Hello from %s!', req.host)});
 });
 
 app.get('/headers', function(req, res) {
