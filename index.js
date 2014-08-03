15881e0fd4d4cd117c478be880bbdf3cdd26ce7c
Joerg Henning <henning.joerg@gmail.com>
2014-08-03 11:48:55 +0700

    Header dump

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', function(req, res) {
   res.status(200).json({msg: 'Hello World!'});
 });
 
+app.get('/headers', function(req, res) {
+  res.status(200).json(req.headers);
+});
+
 app.listen(3000, function() {
   console.log('Here we go...');
 });
